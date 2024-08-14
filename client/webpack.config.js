3feead272559b81f27cdebc17267fe7a8b267821
Aleksei <aakislitsin@gmail.com>
2024-08-14 15:57:11 +0900

    little change in congif

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,49 +1,64 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.proto$/,
-        use: 'protobufjs-loader',
+        use: "protobufjs-loader",
       },
       {
         test: /\.worker\.js$/,
-        use: { loader: 'worker-loader' }
+        use: {
+          loader: "worker-loader",
+          options: {
+            inline: "no-fallback",
+            name: "[name].[contenthash].worker.js",
+            sourceMap: true,
+          },
+        },
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules\/html-entities/,
+        loader: "source-map-loader",
       },
     ],
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/proto/data.proto', to: 'data.proto' }
-      ],
+      patterns: [{ from: "src/proto/data.proto", to: "data.proto" }],
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, "public"),
     },
     compress: true,
     port: 9000,
-  }
+    client: {
+      overlay: false,
+    },
+  },
+  ignoreWarnings: [(warning) => warning.message.includes("source-map-loader")],
 };
